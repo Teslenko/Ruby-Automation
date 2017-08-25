7064ac88c063d830b0477d8aee60ad9d6bf3ad1f
Teslenko <Teslvova@gmail.com>
2017-08-25 11:51:19 +0300

    Update Guide-for-Watir.rb

--- Guide-for-Watir.rb
+++ Guide-for-Watir.rb
@@ -1,4 +1,5 @@
 browser.goto 'https://www.google.com.ua'  - открыть ссылку
+browser.text_field(name: 'q').set 'Vova'  - ввести значение
 browser.link(text: 'Почта').click         - кликнуть по "тексту"
 browser.element(name: 'btnK').hover       - навести курсором на элемент
 browser.quit                              - закрыть браузер
