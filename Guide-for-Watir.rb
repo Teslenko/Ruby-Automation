a04db6444e12b2074d7d70ea42599ca1f1980e62
Teslenko <Teslvova@gmail.com>
2017-08-28 17:50:25 +0300

    Update Guide-for-Watir.rb

--- Guide-for-Watir.rb
+++ Guide-for-Watir.rb
@@ -1,3 +1,7 @@
+require 'watir'
+
+browser = Watir::Browser.new :firefox
+
 browser.goto 'https://www.google.com.ua'  - открыть ссылку
 browser.text_field(name: 'q').set 'Vova'  - ввести значение
 browser.link(text: 'Почта').click         - кликнуть по "тексту"
