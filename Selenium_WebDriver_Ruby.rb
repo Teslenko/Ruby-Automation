eef159ba69b4318ab41772a546fa76c791ad54ba
Teslenko <Teslvova@gmail.com>
2017-08-28 18:42:56 +0300

    Update Selenium_WebDriver_Ruby.rb

--- Selenium_WebDriver_Ruby.rb
+++ Selenium_WebDriver_Ruby.rb
@@ -5,5 +5,6 @@ driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "https://google.ru"
 driver.find_element(:name, 'q').send_keys'watir class'
 driver.find_element(:name, 'btnK').click
+sleep (3)
 driver.find_element(:link_text, 'css - Check tag classes with watir? - Stack Overflow').click
 driver.quit
