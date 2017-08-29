eed3b822befbf0b23315a4f55da1572dad337641
Teslenko <Teslvova@gmail.com>
2017-08-29 11:43:56 +0300

    Update Selenium_WebDriver_Ruby.rb

--- Selenium_WebDriver_Ruby.rb
+++ Selenium_WebDriver_Ruby.rb
@@ -8,4 +8,5 @@ driver.find_element(:name, 'btnK').click                                - кли
 driver.action.move_to(driver.find_element(:link_text, "Почта")).perform - функция "hover"
 sleep (3)                                                               - ожидание
 driver.find_element(:link_text, 'css - Check tag classes with watir? - Stack Overflow').click
+driver.manage.window.move_to(300,400)                                   - переместить окно браузера (координаты)
 driver.quit                                                             - закрыть браузер
