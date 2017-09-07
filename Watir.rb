370ac174fae3c001fbbab14eed93a2d9993df1d7
Teslenko <Teslvova@gmail.com>
2017-09-07 09:51:56 +0300

    Update Watir.rb

--- Watir.rb
+++ Watir.rb
@@ -2,8 +2,8 @@ require 'watir'
 
 browser = Watir::Browser.new :firefox
 
-browser.goto 'https://www.google.com.ua'  - открыть ссылку
-browser.text_field(name: 'q').set 'Vova'  - ввести значение
-browser.link(text: 'Почта').click         - кликнуть по "тексту"
-browser.element(name: 'btnK').hover       - навести курсором на элемент
-browser.quit                              - закрыть браузер
+browser.goto 'https://www.google.com.ua'  #- открыть ссылку
+browser.text_field(name: 'q').set 'Vova'  #- ввести значение
+browser.link(text: 'Почта').click         #- кликнуть по "тексту"
+browser.element(name: 'btnK').hover       #- навести курсором на элемент
+browser.quit                              #- закрыть браузер
